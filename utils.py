1cb35a6fd4bc409d1660d7cfd70b017d998c8333
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2026-05-31 11:22:15 +0200

    Global seed

---
+++ utils.py
@@ -0,0 +1,18 @@
+import random
+import numpy as np
+import torch
+
+def set_global_seed(seed):
+
+    random.seed(seed)
+    np.random.seed(seed)
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    # za determinističko ponašanje (ako koristiš GPU)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    torch.use_deterministic_algorithms(True)
